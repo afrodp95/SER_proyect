02ec330af581cc2e8bef2c5f3b5a35cdf962df2a
Natalia Niño M <natalianinom@MacBook-Pro-de-Natalia.local>
2020-09-24 21:58:28 -0500

    espectrograma

--- preprocessing/0_exploration.py
+++ preprocessing/0_exploration.py
@@ -12,3 +12,12 @@ plt.figure(figsize=(20,5))
 librosa.display.waveplot(x,sr=sr)
 plt.show()
 
+### Hacer espectrograma 
+
+X = librosa.stft(x)
+Xdb = librosa.amplitude_to_db(abs(X))
+plt.figure(figsize=(20, 5))
+librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz')
+plt.colorbar()
+plt.show()
+
