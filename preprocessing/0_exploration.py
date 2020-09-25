07f9b302a04355b41c7310454843d7e0668d4aaf
Andres Rodriguez Perez <afrodp95@outlook.com>
2020-09-24 21:40:58 -0500

    data exploration

--- preprocessing/0_exploration.py
+++ preprocessing/0_exploration.py
@@ -1,2 +1,14 @@
 import librosa
+import os
+import matplotlib.pyplot as plt
+import librosa.display
+
+path = './data/OAF_angry/'
+files = os.listdir(path)
+
+x, sr = librosa.load(path+files[0])
+
+plt.figure(figsize=(20,5))
+librosa.display.waveplot(x,sr=sr)
+plt.show()
 
