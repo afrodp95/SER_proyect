ea2a3cc5c3c766ff2e1779f76c90c32890078f08
davidSarmiento2 <46963679+davidSarmiento2@users.noreply.github.com>
2020-09-26 13:22:41 -0500

    Add files via upload

---
+++ preprocessing/nnTest1.py
@@ -0,0 +1,73 @@
+import time
+import librosa.display
+import matplotlib.pyplot as plt
+from os import listdir
+
+#------------------------------------------------------------------------------
+def main():
+    searchDir()
+
+#------------------------------------------------------------------------------
+def searchDir():
+    myPath = "/home/david/"
+    myPath += "Documentos/Universidad/9no Semestre/Sistemas Inteligentes/Proyecto/TESS Toronto emotional speech set" \
+              " data/"
+
+    for fDir in listdir( myPath ):
+
+        strEmo = getStrEmo( fDir )
+        audioDir = myPath + '/' + fDir
+        for fA in listdir( audioDir ):
+
+            audioPath = audioDir + '/' + fA
+            specto = getSpectrogrm( audioPath )
+            saveImg( specto, strEmo, fA )
+
+
+
+#------------------------------------------------------------------------------
+def getSpectrogrm( audioPath ):
+
+    x, sr = librosa.load( audioPath )
+    X = librosa.stft( x )
+    Xdb = librosa.amplitude_to_db( abs( X ) )
+    plt.figure( figsize=( 20, 5 ) )
+    librosa.display.specshow( Xdb, sr = sr, x_axis="time", y_axis="hz" )
+    plt.colorbar()
+    #plt.show()
+    #plt.savefig(  )
+    time.sleep( 1 )
+
+    return plt
+
+#------------------------------------------------------------------------------
+def saveImg( specto, emotion, nameSpecto ):
+    nameSpecto = nameSpecto[:-1]
+    nameSpecto = nameSpecto[:-1]
+    nameSpecto = nameSpecto[:-1]
+    print(nameSpecto)
+    nameSpecto += "png"
+    newName = emotion + "_" + nameSpecto
+    savePath = "/home/david/Documentos/Universidad/9no Semestre/Sistemas Inteligentes/Proyecto/spectrograms/" + newName
+    specto.savefig( savePath )
+
+#------------------------------------------------------------------------------
+def getStrEmo( name ):
+    if "angry" in name:
+        return "angry"
+    elif "disgust" in name:
+        return "disgust"
+    elif "Fear" in name or "fear" in name:
+        return "fear"
+    elif "happy" in name:
+        return "happy"
+    elif "neutral" in name:
+        return "neutral"
+    elif "surprise" in name:
+        return "surprise"
+    elif "sad" in name or "Sad" in name:
+        return "sad"
+
+#------------------------------------------------------------------------------
+
+main()
