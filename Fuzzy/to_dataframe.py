6d4ca34c1aef057502dab28d7f92e7ebd6931618
Andres Rodriguez Perez <afrodp95@outlook.com>
2020-11-15 16:49:39 -0500

    Data for Fuzzy rules creation

---
+++ Fuzzy/to_dataframe.py
@@ -0,0 +1,16 @@
+import joblib
+import numpy as np 
+import pandas as pd 
+
+ravdess_data = joblib.load( "./Fuzzy/ravdess_speech_data.gz" )
+ravdess_labels = joblib.load( "./Fuzzy/ravdess_numeric_labels.gz" )
+
+
+mapping = {0:"neutral",1:"calm",2:"happy",3:"sad",4:"angry",5:"fearful",6:"disgust",7:"surprised"}
+
+ravdess_data = pd.DataFrame(ravdess_data)
+ravdess_data['emotion'] = ravdess_labels
+ravdess_data['emotion'] = ravdess_data['emotion'].replace(mapping) 
+ravdess_data['emotion'].value_counts()
+
+ravdess_data.to_csv("radvess_data.csv")
